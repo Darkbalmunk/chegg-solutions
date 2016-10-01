ff926d354a9d9334149128d87e9dc5456de81873
Matt Ihlenfield <mtihlenfield@gmail.com>
2016-09-30 21:23:32 -0400

    Figured out how to can an access token

--- chegg.py
+++ chegg.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import requests
+import time
 
-# TODO figure out how to get access token and sessionid
-#   - I'm assuming device id is constant
-# TODO verify that Authorization is constant
+# TODO figure out how to get sessionid
+# TODO figure out how to refresh a token
+# TODO test token.expired method
 
 HEADERS = {
-    "access_token": "",
-    "X-CHEGG-DEVICEID": "",
-    "X-CHEGG-SESSIONID": "",
-    "Authorization": "",
+    # "access_token": "",
+    "X-CHEGG-DEVICEID": ,
+    # "X-CHEGG-SESSIONID": "",
+    "Authorization": ,
     "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.1.1; A0001 Build/LMY48B)",
     "host": "hub.chegg.com",
     "Connection": "Keep-Alive",
@@ -17,8 +18,27 @@ HEADERS = {
 }
 
 
-class Chapter:
+class Token:
+    def __init__(self, chegg_id, access_token, refresh_token, issued_at, expires_in):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.issued_at = issued_at
+        self.expires_in = expires_in
+        self.chegg_id = chegg_id
 
+    def expired(self):
+        current_time = time.time()
+        expiration_time = self.issued_at + self.expires_in
+        if (expiration_time > current_time or (current_time - expiration_time) <= 5):
+            return True
+        else:
+            return False
+
+    def refresh(self):
+        pass
+
+
+class Chapter:
     def __init__(self, id, name, book_isbn, solutions=[]):
         self.solutions = []
         self.id = id
@@ -27,7 +47,6 @@ class Chapter:
 
 
 class Solution:
-
     def __init__(self, id, name, has_solution, chapter_id, book_isbn, steps=[]):
         self.id = id
         self.name = name
@@ -35,6 +54,23 @@ class Solution:
         self.book_isbn = book_isbn
         self.has_solution = has_solution
         self.steps = steps
+
+
+def get_token(username, password):
+    url = "https://hub.chegg.com/oauth/token"
+    data = {
+        "username": username,
+        "password": password,
+        "grant_type": "password"
+    }
+    response = requests.post(url, data=data, headers=HEADERS).json()
+    return Token(
+        access_token=response["access_token"],
+        refresh_token=response["refresh_token"],
+        issued_at=int(response["issued_at"]),
+        expires_in=int(response["expires_in"]),
+        chegg_id=response["chegg_id"]
+    )
 
 
 def get_chapters(isbn):
@@ -51,7 +87,6 @@ def get_chapters(isbn):
             book_isbn=isbn
         )
         chapters += [chapter]
-
     return chapters
 
 
@@ -70,29 +105,33 @@ def get_solutions(chapter):
             book_isbn=chapter.book_isbn
         )
         solutions += [solution]
-
     return solutions
 
 
 def get_steps(solution):
     url = "https://hub.chegg.com/v1/tbs/_/solutions"
 
-    data = {
+    json = {
         "action": "Access",
         "isbn13": solution.book_isbn,
         "problemId": solution.id,
         "userAgent": "Mobile"
     }
 
-    response = requests.post(url, data=data, headers=HEADERS).json()
+    # I must be missing some parameter. It never sends more than 1 step.
+    # Sometimes it sends none
+
+    response = requests.post(url, json=json, headers=HEADERS).json()
     print(response)
 
 
 def main():
+    token = get_token()
+    HEADERS["access_token"] = token.access_token
     isbn = 9781118539712
     chapters = get_chapters(isbn)
     solutions = get_solutions(chapters[0])
-    get_steps(solutions[0])
+    get_steps(solutions[25])
 
 
 if __name__ == "__main__":
